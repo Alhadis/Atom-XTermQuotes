c47889e242640b79790d0efac6f46aa86d6ede99
Alhadis <gardnerjohng@gmail.com>
2018-12-24 15:44:10 +1100

    Remove `.xterm-quotes-disabled' class when deactivating

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const {CompositeDisposable} = require("atom");
+const disableClass = "xterm-quotes-disabled";
 let disposables = null;
 
 module.exports = {
@@ -8,12 +9,13 @@ module.exports = {
 		disposables && disposables.dispose();
 		disposables = new CompositeDisposable(
 			atom.commands.add("atom-workspace", "xterm-quotes:toggle", () => {
-				document.body.classList.toggle("xterm-quotes-disabled");
+				document.body.classList.toggle(disableClass);
 			}),
 		);
 	},
 	
 	deactivate(){
+		document.body.classList.remove(disableClass);
 		if(disposables){
 			disposables.dispose();
 			disposables = null;
